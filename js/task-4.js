6519cfadde349548e4223060620ba93703bdd7ad
Vasyl-Djachuk <djachukvasja@gmail.com>
2023-12-06 09:18:13 +0200

    edited

--- js/task-4.js
+++ js/task-4.js
@@ -6,7 +6,7 @@ const getTotalBalanceByGender = (users, gender) => {
     }, 0);
 };
 
-const allUsers = [
+const allUserss = [
   {
     name: 'Moore Hensley',
     gender: 'male',
@@ -44,6 +44,6 @@ const allUsers = [
   },
 ];
 
-console.log(getTotalBalanceByGender(allUsers, 'male')); // 12053
+console.log(getTotalBalanceByGender(allUserss, 'male')); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, 'female')); // 8863
+console.log(getTotalBalanceByGender(allUserss, 'female')); // 8863
